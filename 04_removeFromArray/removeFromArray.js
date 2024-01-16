96500bcd2dfdf41d3b7a3e434fd34269371ed05b
Brian Stevenson <81999388+BStevens1987@users.noreply.github.com>
2024-01-15 22:14:31 -0800

    Create a function that takes an array and removes listed elements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...trash) {
+  const purgedArray = [];
+  array.forEach((element) => {
+    if (!trash.includes(element)) {
+      purgedArray.push(element);
+    };
+  });
+  return purgedArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;

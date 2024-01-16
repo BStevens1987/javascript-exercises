aa9c896e94df12807e383d962d8a84606ff11ea2
Brian Stevenson <81999388+BStevens1987@users.noreply.github.com>
2024-01-15 20:32:28 -0800

    Create reverseString function that returns its input, reversed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let strAsArray = str.split("");
+  let arrayReversed = strAsArray.reverse();
+  let reversedWithCommas = arrayReversed.toString();
+  let reversed = reversedWithCommas.replace(/,/g, "");
+  return reversed;
 };
 
 // Do not edit below this line

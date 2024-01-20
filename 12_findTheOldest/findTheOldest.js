4be2e1e8fca056ba308b995e86cfc88e0712d2d2
Brian Stevenson <81999388+BStevens1987@users.noreply.github.com>
2024-01-19 21:37:41 -0800

    Create function that returns the oldest and passes 3 tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  return array.reduce((a, b) => {
+    if (a.yearOfDeath === undefined) {
+      a.yearOfDeath = new Date().getFullYear();
+    }
+    else if (b.yearOfDeath === undefined) {
+      b.yearOfDeath = new Date().getFullYear();
+    };
+    if (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth) {
+      return a;
+    }
+    else if (a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth) {
+      return b;
+    };
+  });
 };
 
 // Do not edit below this line

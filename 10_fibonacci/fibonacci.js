88c0ce6d82ccd0e7b5e4906b54970d7880b05037
Brian Stevenson <81999388+BStevens1987@users.noreply.github.com>
2024-01-19 18:59:06 -0800

    Create fibonacci sequence function that passes 11 tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n < 0) {
+    return "OOPS";
+  }
+  else {
+    return Math.floor(((1 + (5)**(1/2))**n - (1 - (5)**(1/2))**n) / (2**n * 5**(1/2)));
+  };
 };
 
 // Do not edit below this line

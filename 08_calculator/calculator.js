fbdc6579b804c7189044e02c3b5635a0d4159cfc
Brian Stevenson <81999388+BStevens1987@users.noreply.github.com>
2024-01-19 17:08:56 -0800

    Create calculator function that passes all sixteen tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	let sum1 = num1 + num2;
+  return sum1;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let difference = num1 - num2;
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for(number of array) {
+    sum += number;
+  };
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+let product = 1;
+for (number of array) {
+  product *= number;
+};
+return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+let powerAnswer = num1 ** num2;
+return powerAnswer;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+let factorialAnswer = 1;
+for(let i = 1; i <= num; i++) {
+  factorialAnswer = factorialAnswer*i;
+}
+return factorialAnswer;
 };
 
 // Do not edit below this line

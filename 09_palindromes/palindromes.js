715c58c7ec6b8686dbf21436f68ce739a6b5e7a5
Brian Stevenson <81999388+BStevens1987@users.noreply.github.com>
2024-01-19 17:57:01 -0800

    Create palindromes function that passes all 7 tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+const strippedString = string.toLowerCase().replace(/[^\w\s]|_/g, "").replace(/ /g, '');
+return strippedString === strippedString.split('').reverse().join('');
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
